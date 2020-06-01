d05e466ec7df6c979fdaaf93b7fb5004eaa38fcc
= <nfelipe.ps@gmail.com>
2020-05-31 21:59:00 -0300

    GoStack 2020 - Module 1 - Challenge 1.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,64 @@ app.use(cors());
 
 const repositories = [];
 
+function validateId(request, response, next) {
+  const { id } = request.params;
+
+  if (isUuid(id)) {
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+    if (repositoryIndex != -1) {
+      request.repositoryIndex = repositoryIndex;
+      return next();
+    }
+  }
+
+  return response.status(400).json({ error: "Repository not found"});
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  
+  const repository = { id: uuid(), title: title, url: url, techs: techs, likes: 0 };
+  
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateId, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repository = repositories[request.repositoryIndex];
+
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+
+  return response.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateId, (request, response) => {
+  const { id } = request.params;
+
+  repositories.splice(request.repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateId, (request, response) => {
+  const { id } = request.params;
+
+  const repository = repositories[request.repositoryIndex];
+
+  repository.likes += 1;
+
+  return response.json(repository);
 });
 
 module.exports = app;
